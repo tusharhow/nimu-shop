6525a0f84327e260f0ec0a39c917060b8e1fda98
tusharhow <tusharsoftdev@gmail.com>
2021-06-26 21:23:19 -0700

    Cart page design fixed

---
+++ lib/constrants.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+const primaryColor = Color(0xFFF293B5F);
+const secondaryColor = Color(0xFF242430);
+const darkColor = Color(0xFF191923);
+const bodyTextColor = Color(0xFF8B8B8D);
+const bgColor = Color(0xFF1E1E28);
+const baseDarkPinkColor = Color(0xff6930c3);
+const baseLightPinkColor = Color(0xff9775fa);
+const baseDarkGreenColor = Color(0xff23bd66);
+const baseLightGreenColor = Color(0xff5dd136);
+const baseDarkOrangeColor = Color(0xffffa601);
+const baseLightCyanColor = Color(0xff05c2bd);
--- lib/views/cart_page.dart
+++ lib/views/cart_page.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:get/get.dart';
+import 'package:nimu_shop/constrants.dart';
 import 'package:nimu_shop/controllers/cart_controller.dart';
 import 'package:nimu_shop/controllers/shop_controller.dart';
+import 'package:velocity_x/velocity_x.dart';
 
 final cartController = Get.put(CartController());
 final shoppingController = Get.put(ShoppingController());
@@ -58,19 +60,125 @@ class _CartPageState extends State<CartPage> {
               );
             },
           ),
-          Container(
-            color: Colors.green,
-            height: 50,
-            width: double.infinity,
-            child: GetX<CartController>(builder: (controller) {
-              return Center(
-                child: Text(
-                  'Total Pricice \$${controller.totalPrice}',
-                  style: TextStyle(color: Colors.white, fontSize: 18),
+          Stack(
+            children: [
+              VxArc(
+                arcType: VxArcType.CONVEX,
+                height: 20,
+                edge: VxEdge.TOP,
+                child: Container(
+                  height: 250,
+                  width: 400,
+                  decoration: BoxDecoration(
+                    color: primaryColor,
+                    // borderRadius: BorderRadius.circular(30),
+                  ),
                 ),
-              );
-            }),
-          )
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 10, vertical: 35),
+                child: Row(
+                  // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.fromLTRB(20, 10, 0, 0),
+                        child: TextField(
+                          decoration: InputDecoration(
+                            labelText: 'Voucher',
+                            labelStyle: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.grey,
+                            ),
+                            enabledBorder: InputBorder.none,
+                            focusedBorder: InputBorder.none,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(30),
+                          color: Colors.green,
+                        ),
+                        height: 45,
+                        child: GetX<CartController>(
+                          builder: (controller) {
+                            return Center(
+                              child: Text(
+                                'Total Pricice \$${controller.totalPrice}',
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 15),
+                              ),
+                            );
+                          },
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Center(
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: 130,
+                    ),
+                    InkWell(
+                      onTap: () {},
+                      child: Container(
+                        height: 40,
+                        width: 300,
+                        child: Center(
+                          child: Text(
+                            'Checkout',
+                            style: TextStyle(
+                              fontSize: 18,
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                        decoration: BoxDecoration(
+                          color: Colors.green,
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    InkWell(
+                      onTap: () {},
+                      child: Container(
+                        height: 40,
+                        width: 300,
+                        child: Center(
+                          child: Text(
+                            'Checkout with PayPal',
+                            style: TextStyle(
+                              fontSize: 18,
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                        decoration: BoxDecoration(
+                          color: Colors.blueAccent,
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ],
       ),
     );
